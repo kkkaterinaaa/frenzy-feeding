8cd9562aa3e0e0b6c431701f37ac3a7534aad2cd
Ekaterina Zaitseva <katy.zaitsewa2207@ya.ru>
2024-03-14 01:49:05 +0300

    fish

--- Assets/Scripts/MouseLookX.cs
+++ Assets/Scripts/MouseLookX.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class MouseLookX : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public float sensitivity = 200f;
     void Update()
     {
-        
+        var horizontalAim = Input.GetAxis("Mouse X") * Time.deltaTime * sensitivity;
+        transform.Rotate(0, horizontalAim, 0);
     }
 }
